c9f2f5d2b90ab0069f5d9d55dd3a690711dec8bd
Rebecca Olson <becca972@gmail.com>
2018-11-29 21:05:01 -0600

    Created login.html and signup.html, signup works

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
-from flask import Flask, request, redirect, render_template
+from flask import Flask, request, redirect, render_template, session, flash
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://blogz:blogzy@localhost:8889/blogz'
 app.config['SQLALCHEMY_ECHO'] = True
+app.secret_key = 'sabrshukr'
 db = SQLAlchemy(app)
 
 
@@ -15,21 +16,107 @@ class Blog(db.Model):
     body = db.Column(db.String(500))
     owner_id = db.Column(db.Integer, db.ForeignKey('user.id'))
 
-    def __init__(self, owner, title, body):
-        self.owner = owner
+    def __init__(self, title, body, owner):
         self.title = title
         self.body = body
+        self.owner = owner
 
 class User(db.Model):
 
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(30))
-    password = db.Column(db.String(25))
+    username = db.Column(db.String(50))
+    password = db.Column(db.String(50))
     blog = db.relationship('Blog', backref='owner')
 
     def __init__(self, username, password):
         self.username = username
         self.password = password
+
+
+#VALIDATION
+
+def improper_length(value):
+    if len(value) < 3:
+        return True
+
+def empty(value):
+    if value == "":
+        return True
+
+
+#LOGIN
+
+@app.route('/login', methods=['POST', 'GET'])
+def login():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        user = User.query.filter_by(username=username).first()
+        if user and user.password == password:
+            session['username'] = username
+            flash("Logged in")
+            return redirect('/')
+        else:
+            flash('User password incorrect, or user does not exist', 'error')
+
+    return render_template('login.html')
+
+#SIGNUP
+
+@app.route('/signup', methods=['POST', 'GET'])
+def signup():
+        
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        verify = request.form['verify']
+
+        username_error = ''
+        password_error = ''
+        verify_error = ''
+    
+        if improper_length(username):
+            username_error = "Username should be at least 3 characters."
+
+        if improper_length(password):
+            password_error = "Password should be at least 3 characters."
+    
+        if password != verify:
+            verify_error = "Passwords must match."
+
+        if empty(username):
+            username_error = "Username cannot be left blank."
+
+        if empty(password):
+            password_error = "Password cannot be left blank."
+
+        if empty(verify):
+            verify_error = "Verify password cannot be left blank."
+
+        if not username_error and not password_error and not verify_error:
+            existing_user = User.query.filter_by(username=username).first()
+            if not existing_user:
+                new_user = User(username, password)
+                db.session.add(new_user)
+                db.session.commit()
+                session['username'] = username
+                flash("You are logged in as {{username}}.")
+                return redirect('/blog')
+            else:
+                flash("That username is already in use. Please choose another username.")
+                return render_template('signup.html')
+
+        else:
+            return render_template('signup.html',
+                username_error = username_error,
+                password_error = password_error,
+                verify_error = verify_error,
+                username = username)
+
+    else:
+        return render_template('signup.html')
+
+#VIEW BLOG
 
 @app.route('/blog', methods=['GET'])
 def index():
@@ -48,20 +135,19 @@ def index():
         return render_template('entry.html',title="Build a Blog",
             entry=entry)
 
+#NEW POST
 
 @app.route('/newpost', methods=['GET'])
 def form():
     return render_template('newpost.html',title="Add a Blog Entry")
-    
-def empty(value):
-    if value == "":
-        return True
+
 
 @app.route('/newpost', methods=['POST'])
 def newpost():
 
     blog_title = request.form['title']
     blog_body = request.form['body']
+    owner = User.query.filter_by(username=session['username']).first()
     
     title_error = ''
     body_error = ''
@@ -73,7 +159,7 @@ def newpost():
         body_error = 'Body cannot be empty.'
 
     if not title_error and not body_error:
-        new_entry = Blog(blog_title, blog_body)
+        new_entry = Blog(blog_title, blog_body, owner)
         db.session.add(new_entry)
         db.session.commit()
         id = new_entry.id
